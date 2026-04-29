bfc08366308216e9dcc64b4367e864c3166a98b4
sailinginmymind <chiscopalexandru97@gmail.com>
2026-04-29 20:30:26 +0200

    Set base path for deployment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/financial-swiss-knife/', 
   plugins: [
     react(),
     tailwindcss(),
